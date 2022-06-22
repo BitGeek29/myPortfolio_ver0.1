bae83c6d400c95a7145cbd4f685ddf383397bb56
BitGeek29 <shaikhhaseeb301@gmail.com>
2022-06-22 18:07:32 -0400

    🎊 myPortFolio

---
+++ constants/data.js
@@ -0,0 +1,89 @@
+const userData = {
+  githubUsername: "BitGeek29",
+  name: "Haseeb Shaikh",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.png",
+  email: "shaikhhaseeb301@gmail.com",
+  phone: "+971 581591732",
+  address: "Sharjah city, United Arab Emirates",
+  projects: [
+    {
+      title: "De-Subsidizer",
+      link: "https://github.com/BitGeek29/Dapp-Subsidy-system-",
+      imgUrl: "/imagesd.jpeg",
+    },
+    {
+      title: "Stackoverload",
+      link: "https://github.com/BitGeek29/HasCode-Stackoverload",
+      imgUrl: "/imagehc.png",
+    },
+    {
+      title: "Chip8-EMu29",
+      link: "https://github.com/BitGeek29/Chip8-EMu29-old-GIT-",
+      imgUrl: "/imagec8.png",
+    },
+    {
+      title: "FlutsApp",
+      link: "https://github.com/BitGeek29/FlutsApp-Express",
+      imgUrl: "/imagefa.png",
+    },
+    {
+      title: "CHAT-SERVER Express",
+      link: "https://github.com/BitGeek29/Express-CHAT-SER",
+      imgUrl: "/imageep.png",
+    },
+    {
+      title: "ToDo-Django",
+      link: "https://github.com/BitGeek29/Django-REST",
+      imgUrl: "/imageDR.png",
+    },
+    {
+      title: "Login Sys-PHP",
+      link: "https://github.com/BitGeek29/php-MySQL",
+      imgUrl: "/imagelp.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `I've been developing full-stack application for last 3 years. I didn't know what full-stack meant at that time back then. I'm so out of content right now that I'm literally crying writing this text since lorem text.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a legend. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+      'I tell computers what to do. Tell them the right things and the results can be extraordinary. I am a Computer Science graduate with a passion for all things tech. I enjoy using my ever expanding knowledge of software development and data analytics to tackle everyday problems.',
+    ],
+    //currentProject: null,
+    //currentProjectUrl: null,
+  },
+  experience: [
+    {
+      title: "Software Developer",
+      company: "SNA Infotech",
+      year: "2021",
+      companyLink: "https://mroads.com",
+      desc: "Contributed to our flagship product - Panna - which is an AI powered candidate interviewing product.",
+    },
+    {
+      title: "Graduation",
+      company: "Mumbai University",
+      year: "2022",
+      companyLink: "https://mu.ac.in/",
+      desc: "Major in Computer Science and Bachelor's of Science with a CGPI of 7.9. Nobody asks this but it's okay.",
+    },
+    {
+      title: "Junior College",
+      company: "Maharastra State Board Of Higher Secondary Education (Pune)",
+      year: "2019",
+      companyLink: "http://www.huzaifasjc.com/",
+      desc: "Subjects being PCM - I barely survived with 6.1 marks aggregate. Flex fridays, fellas.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1Geryx3Imvj0vJyKR8aySrwD8RY0rhW8e/view?usp=sharing",
+  socialLinks: {
+    linkedin: "https://www.linkedin.com/in/bitgeek29/",
+    github: "https://github.com/BitGeek29",
+  },
+};
+
+export default userData;
